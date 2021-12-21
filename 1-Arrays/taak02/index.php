7727d235646d3ce828ca45b65a7a51c4dce8967b
vascoooveres <90618860+vascoooveres@users.noreply.github.com>
2021-12-21 13:05:10 +0100

    opdracht 2 php

---
+++ 1-Arrays/taak02/index.php
@@ -0,0 +1,8 @@
+<?php
+$persoon["voornaam"]   = "Vasco";
+$persoon["achternaam"] = "Overes";  
+$persoon["leeftijd"]   = 16;
+$persoon["opleiding"] = "software developer";
+
+echo $persoon["voornaam"] . " " . $persoon["achternaam"] . " " . "is" . " " . $persoon["leeftijd"] . " " . "jaar oud en doet de opleiding" . " " . $persoon["opleiding"];
+?>
