81a0402553f9befa1039a9932f5976431a0eabac
vascoooveres <90618860+vascoooveres@users.noreply.github.com>
2021-12-21 12:27:21 +0100

    opdracht 1 php lvl 2

---
+++ 1-Arrays/taak01/index.php
@@ -0,0 +1,11 @@
+<?php
+$student= array(
+    "naam" => "Vasco",
+    "achternaam" => "Overes",
+    "klas" => "LCTSDO1D",
+    "leeftijd" => 16
+
+);
+
+echo "Mijn naam is" . " " . $student['naam'] . " " . "mijn achternaam is" . " " . $student['achternaam'] . " " . "ik zit in klas" . " " . $student['klas'] . " " . "ik ben" . " " . $student['leeftijd'] . " " . "jaar oud.";
+?>
