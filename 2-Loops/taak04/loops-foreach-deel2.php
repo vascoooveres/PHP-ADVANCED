3dd054aa9fc774da33be99a926b5c90a9a35891a
vascoooveres <90618860+vascoooveres@users.noreply.github.com>
2022-01-10 11:53:57 +0100

    taak5

---
+++ 2-Loops/taak04/loops-foreach-deel2.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $klassen = array("8A", "8B", "8C", "8D", "8E", "9A", "9B", "9C", "9D", "9E");
+        
+    ?>
+</body>
+</html>
---
+++ 2-Loops/taak05/loops-foreach-deel3.php
@@ -0,0 +1,12 @@
+<?php
+$student  = array(
+    "voornaam"    => "Jan",
+    "achternaam"  => "Janssen",
+    "klas"        => "9A",
+    "Leeftijd"    => 17,
+    "Woonplaats"  => "Amstelveen"
+  );
+  foreach($student as $key => $value){
+    echo $key . ": " . $value . "<br>";
+}
+?>
