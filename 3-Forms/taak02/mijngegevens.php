ab0c302534103bf2f79ab1215da4c04d49bdb59a
vascoooveres <90618860+vascoooveres@users.noreply.github.com>
2022-01-13 13:41:47 +0100

    opdracht forms

---
+++ 3-Forms/taak02/mijngegevens.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    </head>
+    <?php
+    echo "<body style=background:" .$_POST['kleuren'].";>";
+    echo $_POST["Voornaam"] . "</br>";
+    echo $_POST["Achternaam"] . "</br>";
+    echo $_POST["klas"] . "</br>";
+    echo $_POST["leeftijd"] . "</br>";
+    echo $_POST["adres"] . "</br>";
+    echo $_POST["Plaatsnaam"] . "</br>";
+    echo $_POST["geslacht"] . "</br>";
+    // echo "<style='background:".$_POST['kleuren'].">";
+echo "</body>";
+    ?>
+</html>
