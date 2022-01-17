36a1b5e10786f13dd512a831412101fa86b3f2bb
vascoooveres <90618860+vascoooveres@users.noreply.github.com>
2022-01-17 14:48:21 +0100

    get opdracht

--- 3-Forms/taak03/favorieteNummer.php
+++ 3-Forms/taak03/favorieteNummer.php
@@ -2,4 +2,5 @@
 foreach($_GET as $key => $value){
     echo $value . "<br>"; 
 }
+/*http://localhost/opdrachten%20school/PHP-ADVANCED/3-Forms/taak03/favorieteNummer.php?artiest=ada&titel=ad&Jaar=asd&genre=asd*/
 ?>
